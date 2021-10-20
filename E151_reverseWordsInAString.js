0c099d8e4de1f7d4f124db8c70a8d4c62bfa9ef7
Ezekiel Yu <cellozeke@gmail.com>
2021-10-20 15:57:56 -0700

    first solution E151

---
+++ E151_reverseWordsInAString.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+// .load E151_reverseWordsInAString.js
+
+var reverseWords = function(s) {
+  let word = ''
+  let words = []
+  for (let i = 0; i < s.length; i++) {
+    if (word === '' && s[i] === ' ') {
+      continue
+    }
+    if (s[i] !== ' ') {
+      word += s[i]
+      if (i === s.length - 1) words.push(word)
+    } else {
+      words.push(word)
+      word = ''
+    }
+  }
+  return words.reverse().join(' ')
+};
+
+console.log(reverseWords('hi i am happy'))
