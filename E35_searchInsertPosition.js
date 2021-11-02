1c8709b6199e5ecc5c1a2ad14c4e38dfd0b03c18
Ezekiel Yu <cellozeke@gmail.com>
2021-11-01 20:22:00 -0700

    started search insert position

---
+++ E35_searchInsertPosition.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  if (!nums.length) return 0;
+  let mid = Math.floor(nums.length / 2);
+  if (nums[mid] === target) return mid;
+  if (nums.length === 1) return target > nums[0] ? 1 : 0; 
+  if (nums[mid] < target) return mid + searchInsert(nums.slice(mid));
+  return searchInsert(nums.slice(0, mid))
+};
+
+//[1, 3, 5, 6], 2
