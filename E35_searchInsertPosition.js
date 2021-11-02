358ede1d4d32c118e749d227f8862b920a8d9c2f
Ezekiel Yu <cellozeke@gmail.com>
2021-11-01 20:51:11 -0700

    solved search insert position (81%)

--- E35_searchInsertPosition.js
+++ E35_searchInsertPosition.js
@@ -8,8 +8,8 @@ var searchInsert = function(nums, target) {
   let mid = Math.floor(nums.length / 2);
   if (nums[mid] === target) return mid;
   if (nums.length === 1) return target > nums[0] ? 1 : 0; 
-  if (nums[mid] < target) return mid + searchInsert(nums.slice(mid));
-  return searchInsert(nums.slice(0, mid))
+  if (nums[mid] < target) return mid + searchInsert(nums.slice(mid), target);
+  return searchInsert(nums.slice(0, mid), target)
 };
 
 //[1, 3, 5, 6], 2
