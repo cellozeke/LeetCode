a8840f2c7b1a7f3aabca05d9d9c92efe7b773bbe
Ezekiel Yu <cellozeke@gmail.com>
2021-11-30 21:53:28 -0800

    solved happy number

--- E202_happyNumber.js
+++ E202_happyNumber.js
@@ -3,5 +3,19 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-    
+  debugger;
+  let hash = {};
+  while (!hash[n]) {
+    hash[n] = 1;
+    let sum = 0;
+    while (n) {
+      let m = n % 10;
+      sum += (m * m);
+      // if (m === n) break;
+      n = (n - m) / 10;
+    }
+    if (Number.isInteger(Math.log10(sum))) return true;
+    n = sum;
+  }
+  return false;
 };
