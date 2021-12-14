b020c1b96106227f12139844e70858f403159a38
Ezekiel Yu <cellozeke@gmail.com>
2021-12-13 21:30:13 -0800

    started number of 1 bits

---
+++ E191_numberOfOneBits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+  let str = n.toString();
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '1') count++;
+  }
+  return count;
+};
